4fdf8a32f5c45b0f5640b2deb1e6968fe9cc14ba
Humberto Caetano <humberto.ccs@gmail.com>
2024-03-28 19:54:45 -0300

    Nova linha adicionada no app.js

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require('express');
 const app = express();
 app.get('/', function (req, res) {
     res.send('Hello Geeks!');
+    res.send('<br>');
+    res.send('Nova Linha!');
 });
 app.listen(3000, function () {
     console.log('Servidor na porta 3000!');
